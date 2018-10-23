17a6f5f0ba326cb35840c9320172c8e50db25022
Otho Mantegazza <otho.mantegazza@gmail.com>
2018-10-23 16:10:32 +0200

    Draft function that normalize Ct

--- R/normalize-scale.R
+++ R/normalize-scale.R
@@ -8,3 +8,35 @@
 gm_mean <-  function(x, na.rm=TRUE){
   exp(sum(log(x[x > 0]), na.rm=na.rm) / length(x))
 }
+
+#' Normalize Ct Values
+#'
+#' Normalize Ct values in each sample on the geometric mean of user provided
+#' reference genes.
+#'
+#' @export
+
+normalize  <- function(.data, normalizers)
+{
+ stopifnot(all(normalizers %in% .data$target_name))
+
+  # The geometric mean of normalizers
+  # estimates the amount of cDNA in the sample
+  norms <-
+    .data %>%
+    dplyr::filter(target_name %in% normalizers) %>%
+    dplyr::group_by(sample_name) %>%
+    dplyr::summarize(norm_geom_mean = gm_mean(ct_value))
+
+
+  # subtract normalizer and take exponential to estimate expression
+  # note!!! Skip calibration, is it legit????
+  # So the formula is 2^-(Ct_gene - Ct_norm)
+  norm_data <-
+    .data %>%
+    dplyr::filter(!target_name %in% normalizers) %>%
+    dplyr::left_join(norms) %>%
+    dplyr::mutate(expression = 2^(-(ct_value - norm_geom_mean))) %>%
+    # Low expressed genes (Ct 999) to 0
+    dplyr::mutate(expression = round(expression, digits = 5))
+}
